3cf201a7b0c06a40d9778cc44aa658305bcf9422
rsookram <rashadsookram@gmail.com>
2024-09-20 18:02:04 -0400

    Write to file system directly
    instead of using SharedPreferences

--- src/main/java/io/github/rsookram/scratchpad/Storage.java
+++ src/main/java/io/github/rsookram/scratchpad/Storage.java
@@ -1,21 +1,38 @@
 package io.github.rsookram.scratchpad;
 
 import android.content.Context;
-import android.content.SharedPreferences;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class Storage {
 
-    private final SharedPreferences sharedPreferences;
+    private final Context context;
 
     public Storage(Context context) {
-        this.sharedPreferences = context.getSharedPreferences("note", Context.MODE_PRIVATE);
+        this.context = context;
     }
 
     public String load(boolean main) {
-        return sharedPreferences.getString(main ? "n" : "n2", "");
+        String fileName = main ? "n" : "n2";
+        try (InputStream is = context.openFileInput(fileName)) {
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (FileNotFoundException e) {
+            return "";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void save(String value, boolean main) {
-        sharedPreferences.edit().putString(main ? "n" : "n2", value).apply();
+        String fileName = main ? "n" : "n2";
+        try (OutputStream os = context.openFileOutput(fileName, Context.MODE_PRIVATE)) {
+            os.write(value.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
